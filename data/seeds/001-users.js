72dc9e3e00c4be0f226e6c12ccf06c45e4cb681c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2019-11-19 11:18:45 -0600

    new seed run

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,31 +4,31 @@ const bcrypt = require("bcryptjs");
 exports.seed = function(knex, Promise) {
   return knex("users").insert([
     {
-      username: "Seema1",
+      username: "Seema2",
       password: bcrypt.hashSync("random", 14),
       email: "seema@gmail.com",
       location: "Nebraska, NE"
     },
     {
-      username: "Fatima1",
+      username: "Fatima2",
       password: bcrypt.hashSync("random", 14),
       email: "fatima@gmail.com",
       location: "Nebraska, NE"
     },
     {
-      username: "Asima1",
+      username: "Asima2",
       password: bcrypt.hashSync("random", 14),
       email: "asima@gmail.com",
       location: "Nebraska, NE"
     },
     {
-      username: "Chris1",
+      username: "Chris2",
       password: bcrypt.hashSync("random", 14),
       email: "chris@gmail.com",
       location: "Nebraska, NE"
     },
     {
-      username: "Meena1",
+      username: "Meena2",
       password: bcrypt.hashSync("random", 14),
       email: "meena@gmail.com",
       location: "Nebraska, NE"
